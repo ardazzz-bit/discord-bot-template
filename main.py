00bb39ab3b3e78d8dab295632b46a33da1015425
ardazzz-bit <anasosamaqr14@gmail.com>
2025-08-07 16:13:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import discord
+from discord import app_commands
+import os
+
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
+
+    async def on_ready(self):
+        await self.tree.sync()
+        print(f'Logged in as {self.user}!')
+
+client = MyClient()
+
+@client.tree.command(name="ping", description="Replies with Pong!")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+
+client.run(os.getenv("TOKEN"))
 
